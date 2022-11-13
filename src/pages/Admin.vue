87f5e08fba229322d02fe9480bff301ad476960c
MSandoval <matias.mbsa1876@gmail.com>
2022-11-13 01:38:46 -0300

    commit

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -1,7 +1,7 @@
 <!--- #ESTA ERA LA VISTA ANTERIOR QUE DEJE EN COMENTARIO PARA INTENTAR IMPLEMENTAR LA Q-TABLE
     <template>div class="row text-left">
     <div class="col-6 ">
-        <span class="text-weight-bolder">Usuario</span> 
+        <span class="text-weight-bolder">Usuario</span>
         <div>
             <div class="q-pa-md q-gutter-sm">
                 Armando Meza
@@ -9,13 +9,13 @@
         </div>
     </div>
     <div class="col-2">
-        <span class="text-weight-bolder">Rol</span> 
+        <span class="text-weight-bolder">Rol</span>
         <div>
             Rol
         </div>
     </div>
     <div class="col-4">
-        <span class="text-weight-bolder">Acciones</span> 
+        <span class="text-weight-bolder">Acciones</span>
         <div>
             <div class="q-pa-md q-gutter-sm">
                 <q-btn dense square color="green" icon="edit" />
@@ -23,22 +23,27 @@
             </div>
         </div>
     </div>
-</div>  
+</div>
 </template>
 --->
   <template>
-    <div class="q-pa-md">
-      <q-table
-        title="Treats"
-        :rows="rows"
-        :columns="columns"
-        row-key="name"
-      />
-    </div>
+    <q-page>
+      <div class="q-pa-md">
+        <q-table
+          title="Treats"
+          :rows="rows"
+          :columns="columns"
+          row-key="name"
+       />
+      </div>
+    <lista-ayudantias :ayudantias="ayudantias"/>
+  </q-page>
   </template>
-  
-  <script>
-  const columns = [
+
+<script>
+import ListaAyudantias from 'src/components/ListaAyudantias.vue'
+
+const columns = [
     { name: 'name', required: true, label: 'Dessert (100g serving)', align: 'left', field: row => row.name, format: val => `${val}`, sortable: true },
     { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
     { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
@@ -47,22 +52,20 @@
     { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
     { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
     { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
-  ]
-  
-  const rows = [
-   
-  ]
-  
-  export default {
-    setup () {
-      return {
-        columns,
-        rows
-      }
-    }
-  }
-  </script>
-  <script>
+]
 
-  </script>
-  
+const rows = [
+
+]
+
+export default {
+  components: { ListaAyudantias },
+  setup () {
+     return {
+      columns,
+      rows
+     }
+   }
+}
+</script>
+
