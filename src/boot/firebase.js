3d01c16b8964de093a12ba3bea5c7a25e8871adc
MSandoval <matias.mbsa1876@gmail.com>
2022-11-17 00:16:24 -0300

    commit

--- src/boot/firebase.js
+++
@@ -1,8 +0,0 @@
-import { boot } from 'quasar/wrappers'
-
-console.log('firebase ok');
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async (/* { app, router, ... } */) => {
-  // something to do
-})
