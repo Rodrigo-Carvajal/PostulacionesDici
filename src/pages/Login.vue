99acfcc2db8338d80da3a130184aba1591e0d178
Rodrigo-Carvajal <rodrigo.carvajal.ramos@gmail.com>
2022-11-23 02:37:29 -0300

    Se agrega el back end del login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,156 +3,151 @@
     class="aaa window-height window-width row justify-center items-center"
   >
     <q-header>
-      <q-btn label="Profe" to="/profesor" type="button" color="primary"/>
-      <q-btn label="Estudiante" to="/estudiante" type="button" color="primary"/>
-      <q-btn label="Admin" to="/admin" type="button" color="primary"/>
+      <q-btn label="Profe" to="/profesor" type="button" color="primary" />
+      <q-btn
+        label="Estudiante"
+        to="/estudiante"
+        type="button"
+        color="primary"
+      />
+      
     </q-header>
-      <div class="column q-pa-lg">
-        <div class="row">
-          <q-card square class="shadow-24" style="width:auto ;height:auto;">
-            <q-card-section class="bg-cyan-10">
-              <h6 class="text-h6 text-center text-white q-ma-md">Ayudantías DICI</h6>
-            </q-card-section>
-            <q-card-section>
-              <q-form class="q-px-sm q-pt-m">
-                <q-input
+    <div class="column q-pa-lg">
+      <div class="row">
+        <q-card square class="shadow-24" style="width: auto; height: auto">
+          <q-card-section class="bg-cyan-10">
+            <h6 class="text-h6 text-center text-white q-ma-md">
+              Ayudantías DICI
+            </h6>
+          </q-card-section>
+          <q-card-section>
+            <q-form class="q-px-sm q-pt-m">
+              <q-input
                 square
                 v-model="email1"
                 type="email"
                 label="Correo institucional"
                 lazy-rules
                 :rules="[
-                  val => val && val.length > 0 || 'Este campo es obligatorio',
-
+                  (val) =>
+                    (val && val.length > 0) || 'Este campo es obligatorio',
                 ]"
-                >
-                  <template v-slot:prepend>
-                    <q-icon name="email" />
-                  </template>
-                </q-input>
-                
-                <q-input 
+              >
+                <template v-slot:prepend>
+                  <q-icon name="email" />
+                </template>
+              </q-input>
+
+              <q-input
                 square
-                v-model="password1" 
-                type="password" 
-                label="Contraseña"                
+                v-model="password1"
+                type="password"
+                label="Contraseña"
                 lazy-rules
                 :rules="[
-                  val => val && val.length > 0 || 'Este campo es obligatorio',
-
-                ]">
-                  <template v-slot:prepend>
-                    <q-icon name="lock" />
-                  </template>
-                </q-input>
-              </q-form>
+                  (val) =>
+                    (val && val.length > 0) || 'Este campo es obligatorio',
+                ]"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+              </q-input>
+            </q-form>
+          </q-card-section>
+          <q-card-section>
+            <div class="text-center q-pa-md q-gutter-md"></div>
+          </q-card-section>
+          <q-card-actions class="q-px-xs flex-center">
+            <q-card-section>
+              <q-btn
+                unelevated
+                size="l"
+                icon="login"
+                color="cyan-10"
+                label="Iniciar sesión"
+                @click="login(email1, password1)"
+              />
             </q-card-section>
             <q-card-section>
-              <div class="text-center q-pa-md q-gutter-md">
-
-                <q-btn-toggle
-                  v-model="rol1"
-                  toggle-color="cyan-5"
-                  :options="[
-                    {label: 'Administrador', value: 'Administrador'},
-                    {label: 'Profesor', value: 'Profesor'},
-                    {label: 'Estudiante', value: 'Estudiante'}
-                    ]"
-                >
-                </q-btn-toggle>
-              </div>
+              <q-btn
+                unelevated
+                outline
+                size="l"
+                color="cyan-10"
+                to="/signin"
+                class="full-width text-white"
+                label="¿No tienes cuenta?"
+              />
             </q-card-section>
-            <q-card-actions class="q-px-xs flex-center">
-              <q-card-section>
-                <q-btn 
-                unelevated 
-                size="l" 
-                icon="login" 
-                color="cyan-10"  
-                label="Iniciar sesión" 
-                @click="login(email1,password1)"/>
-              </q-card-section>
-              <q-card-section>
-                <q-btn 
-                unelevated 
-                outline 
-                size="l"  
-                color="cyan-10" 
-                to="/signin" 
-                class="full-width text-white" 
-                label="¿No tienes cuenta?"/>
-              </q-card-section>
-
-            </q-card-actions>
-          </q-card>
-        </div>
+          </q-card-actions>
+        </q-card>
       </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { ref } from 'vue';
-import {signInWithEmailAndPassword} from "firebase/auth";
-import { auth } from 'src/boot/firebase';
-import { collection } from 'firebase/firestore';
-import { db } from "boot/firebase"
-import { getDocs, addDoc } from 'firebase/firestore';
+import { ref } from "vue";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "src/boot/firebase";
+import { collection } from "firebase/firestore";
+import { db } from "boot/firebase";
+import { getDocs, addDoc } from "firebase/firestore";
 
 export default {
-  name: 'Login',
-  components:{
-
-  },
-  data: function() {
+  name: "Login",
+  components: {},
+  data: function () {
     return {
-      email1: '',
-      password1: '',
+      email1: "",
+      password1: "",
       rol1: ref(null),
-    }
+    };
   },
-  methods:{
-    login(email1,password1){
-      signInWithEmailAndPassword(auth, email1, password1)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        if (this.rol1 == "Profesor"){
-          window.location.assign("#/profesor");
-
-        }
-        else if(this.rol1 == "Estudiante") {
-          window.location.assign("#/estudiante");
-        }
-
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
+  methods: {
+    async login(email1, password1) {
+          signInWithEmailAndPassword(auth, email1, password1)
+        .then((userCredential) => {
+          
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        }); 
+        console.log(this.email1)
+          const obtenerTodosDocumentos = await getDocs(
+          collection(db, "usuarios"),
+          );
+          obtenerTodosDocumentos.forEach((doc) => {
+            if ((this.email1 == doc.data().email)) {
+              if (doc.data().rol == "Profesor") {
+                window.location.assign("#/profesor");
+                console.log("🚀 ~ file: Login.vue ~ line 129 ~ obtenerTodosDocumentos.forEach ~ Profesor", )
+              } else if (doc.data().rol == "Estudiante") {
+                window.location.assign("#/estudiante");
+                console.log("🚀 ~ file: Login.vue ~ line 129 ~ obtenerTodosDocumentos.forEach ~ estudiante", )
+              } else if (doc.data().rol == "Registrado"){
+                window.location.assign("/#/registrado")
+              }
+              else{alert('sin rol estudiante/profesor')}
+            }
+          })
+      
     },
-    async obtenerRoles(email1){
-      try {
-        const obtenerTodosDocumentos = await getDocs(collection(db, "usuarios"));
-        obtenerTodosDocumentos.forEach((doc) => {
-        console.log(doc.id, " => ", doc.data());
-      });
-      } catch (error) {
-        console.log(error);
-      }
-    }
+
   },
-  created(){
-    this.obtenerRoles();
-  }
-}
+  created() {
+  },
+};
 </script>
 <style>
 .aaa {
-  background-color: #F2FF51;
+  background-color: #f2ff51;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25'%3E%3Cdefs%3E%3ClinearGradient id='a' gradientUnits='userSpaceOnUse' x1='0' x2='0' y1='0' y2='100%25' gradientTransform='rotate(227,908,505)'%3E%3Cstop offset='0' stop-color='%23F2FF51'/%3E%3Cstop offset='1' stop-color='%230CADB0'/%3E%3C/linearGradient%3E%3Cpattern patternUnits='userSpaceOnUse' id='b' width='300' height='250' x='0' y='0' viewBox='0 0 1080 900'%3E%3Cg fill-opacity='0.19'%3E%3Cpolygon fill='%23444' points='90 150 0 300 180 300'/%3E%3Cpolygon points='90 150 180 0 0 0'/%3E%3Cpolygon fill='%23AAA' points='270 150 360 0 180 0'/%3E%3Cpolygon fill='%23DDD' points='450 150 360 300 540 300'/%3E%3Cpolygon fill='%23999' points='450 150 540 0 360 0'/%3E%3Cpolygon points='630 150 540 300 720 300'/%3E%3Cpolygon fill='%23DDD' points='630 150 720 0 540 0'/%3E%3Cpolygon fill='%23444' points='810 150 720 300 900 300'/%3E%3Cpolygon fill='%23FFF' points='810 150 900 0 720 0'/%3E%3Cpolygon fill='%23DDD' points='990 150 900 300 1080 300'/%3E%3Cpolygon fill='%23444' points='990 150 1080 0 900 0'/%3E%3Cpolygon fill='%23DDD' points='90 450 0 600 180 600'/%3E%3Cpolygon points='90 450 180 300 0 300'/%3E%3Cpolygon fill='%23666' points='270 450 180 600 360 600'/%3E%3Cpolygon fill='%23AAA' points='270 450 360 300 180 300'/%3E%3Cpolygon fill='%23DDD' points='450 450 360 600 540 600'/%3E%3Cpolygon fill='%23999' points='450 450 540 300 360 300'/%3E%3Cpolygon fill='%23999' points='630 450 540 600 720 600'/%3E%3Cpolygon fill='%23FFF' points='630 450 720 300 540 300'/%3E%3Cpolygon points='810 450 720 600 900 600'/%3E%3Cpolygon fill='%23DDD' points='810 450 900 300 720 300'/%3E%3Cpolygon fill='%23AAA' points='990 450 900 600 1080 600'/%3E%3Cpolygon fill='%23444' points='990 450 1080 300 900 300'/%3E%3Cpolygon fill='%23222' points='90 750 0 900 180 900'/%3E%3Cpolygon points='270 750 180 900 360 900'/%3E%3Cpolygon fill='%23DDD' points='270 750 360 600 180 600'/%3E%3Cpolygon points='450 750 540 600 360 600'/%3E%3Cpolygon points='630 750 540 900 720 900'/%3E%3Cpolygon fill='%23444' points='630 750 720 600 540 600'/%3E%3Cpolygon fill='%23AAA' points='810 750 720 900 900 900'/%3E%3Cpolygon fill='%23666' points='810 750 900 600 720 600'/%3E%3Cpolygon fill='%23999' points='990 750 900 900 1080 900'/%3E%3Cpolygon fill='%23999' points='180 0 90 150 270 150'/%3E%3Cpolygon fill='%23444' points='360 0 270 150 450 150'/%3E%3Cpolygon fill='%23FFF' points='540 0 450 150 630 150'/%3E%3Cpolygon points='900 0 810 150 990 150'/%3E%3Cpolygon fill='%23222' points='0 300 -90 450 90 450'/%3E%3Cpolygon fill='%23FFF' points='0 300 90 150 -90 150'/%3E%3Cpolygon fill='%23FFF' points='180 300 90 450 270 450'/%3E%3Cpolygon fill='%23666' points='180 300 270 150 90 150'/%3E%3Cpolygon fill='%23222' points='360 300 270 450 450 450'/%3E%3Cpolygon fill='%23FFF' points='360 300 450 150 270 150'/%3E%3Cpolygon fill='%23444' points='540 300 450 450 630 450'/%3E%3Cpolygon fill='%23222' points='540 300 630 150 450 150'/%3E%3Cpolygon fill='%23AAA' points='720 300 630 450 810 450'/%3E%3Cpolygon fill='%23666' points='720 300 810 150 630 150'/%3E%3Cpolygon fill='%23FFF' points='900 300 810 450 990 450'/%3E%3Cpolygon fill='%23999' points='900 300 990 150 810 150'/%3E%3Cpolygon points='0 600 -90 750 90 750'/%3E%3Cpolygon fill='%23666' points='0 600 90 450 -90 450'/%3E%3Cpolygon fill='%23AAA' points='180 600 90 750 270 750'/%3E%3Cpolygon fill='%23444' points='180 600 270 450 90 450'/%3E%3Cpolygon fill='%23444' points='360 600 270 750 450 750'/%3E%3Cpolygon fill='%23999' points='360 600 450 450 270 450'/%3E%3Cpolygon fill='%23666' points='540 600 630 450 450 450'/%3E%3Cpolygon fill='%23222' points='720 600 630 750 810 750'/%3E%3Cpolygon fill='%23FFF' points='900 600 810 750 990 750'/%3E%3Cpolygon fill='%23222' points='900 600 990 450 810 450'/%3E%3Cpolygon fill='%23DDD' points='0 900 90 750 -90 750'/%3E%3Cpolygon fill='%23444' points='180 900 270 750 90 750'/%3E%3Cpolygon fill='%23FFF' points='360 900 450 750 270 750'/%3E%3Cpolygon fill='%23AAA' points='540 900 630 750 450 750'/%3E%3Cpolygon fill='%23FFF' points='720 900 810 750 630 750'/%3E%3Cpolygon fill='%23222' points='900 900 990 750 810 750'/%3E%3Cpolygon fill='%23222' points='1080 300 990 450 1170 450'/%3E%3Cpolygon fill='%23FFF' points='1080 300 1170 150 990 150'/%3E%3Cpolygon points='1080 600 990 750 1170 750'/%3E%3Cpolygon fill='%23666' points='1080 600 1170 450 990 450'/%3E%3Cpolygon fill='%23DDD' points='1080 900 1170 750 990 750'/%3E%3C/g%3E%3C/pattern%3E%3C/defs%3E%3Crect x='0' y='0' fill='url(%23a)' width='100%25' height='100%25'/%3E%3Crect x='0' y='0' fill='url(%23b)' width='100%25' height='100%25'/%3E%3C/svg%3E");
   background-attachment: fixed;
   background-size: cover;
-
 }
 </style>
